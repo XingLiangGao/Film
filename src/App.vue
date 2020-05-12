320a3b378d881918689c720ec1e8ff0cb6015144
gyxyl <15201636144@163.com>
2020-05-12 16:18:36 +0800

    vue class类插件

--- src/App.vue
+++ src/App.vue
@@ -6,15 +6,15 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
+import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from './components/HelloWorld.vue';
 
-export default Vue.extend({
-  name: 'App',
+@Component({
   components: {
-    HelloWorld,
-  },
-});
+    HelloWorld
+  }
+})
+export default class App extends Vue {}
 </script>
 
 <style lang="scss">
